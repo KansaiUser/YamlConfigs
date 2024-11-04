ee39ca44ffd21186ad0a5231226d6d7e7c3cf891
hitoruna <hitorunajp@gmail.com>
2024-11-04 17:22:19 +0900

    applied strict yaml and pydantic

--- source/process_cars.py
+++ source/process_cars.py
@@ -1,8 +1,43 @@
-import yaml
+from pydantic import BaseModel, conlist, ValidationError
+from strictyaml import YAML, load,YAMLError
+# import strictyaml as sy
+from typing import Optional
+from pathlib import Path
 
-def load_car_data(filename="car_data.yaml"):
+# Define a Pydantic model for car data
+class CarData(BaseModel):
+    model: str
+    length: float
+    width: float
+    height: float
+    fuel_efficiency: list[float]
+
+class CarsData(BaseModel):
+    listCar : list[CarData]
+
+
+def load_car_data(filename:Path="car_data.yaml") -> YAML:
     with open(filename, "r") as file:
-        return yaml.safe_load(file)
+        try:
+            yaml_data = load(file.read())
+            return yaml_data    
+        except YAMLError as e:
+            print(f"Error parsing YAML file: {e}")
+        except ValidationError as e:
+            print(f"Data validation error: {e}")
+
+def validate_config(parsed_config: YAML=None) -> Optional[CarsData]:
+    if parsed_config is None:
+        print("Parsed config is None")
+        return None
+
+    try:
+        # Convert YAML data into CarsData model
+        car_list_data = [CarData(**item) for item in parsed_config.data]
+        return CarsData(listCar=car_list_data)
+    except ValidationError as e:
+        print(f"Data validation error: {e}")
+        return None
 
 def calculate_volume(length, width, height):
     return length * width * height
@@ -11,14 +46,18 @@ def calculate_average_fuel_efficiency(fuel_efficiency):
     return sum(fuel_efficiency) / len(fuel_efficiency)
 
 def main():
-    car_data = load_car_data()
+    car_data = validate_config(load_car_data())
+    
+    if car_data is None:
+        return
+
     total_volume = 0
-    for car in car_data:
-        model = car["model"]
-        length = car["length"]
-        width = car["width"]
-        height = car["height"]
-        fuel_efficiency = car["fuel_efficiency"]
+    for car in car_data.listCar:
+        model = car.model
+        length = car.length
+        width = car.width
+        height = car.height
+        fuel_efficiency = car.fuel_efficiency
 
         # Calculate the volume for the current car
         volume = calculate_volume(length, width, height)
@@ -32,7 +71,7 @@ def main():
         print(f"The average fuel efficiency of {model} is {avg_fuel_efficiency:.2f} km/l.")
 
     # Calculate the average volume across all cars
-    average_volume = total_volume / len(car_data)
+    average_volume = total_volume / len(car_data.listCar)
     print(f"\nThe average volume of the cars is {average_volume:.2f} cubic meters.")
 
 if __name__ == "__main__":
